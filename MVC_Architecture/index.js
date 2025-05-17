6d5628e15110afca4cba8583d39f6d13d28c2d05
Sadiq <mdsadiq1221@gmail.com>
2025-05-17 15:40:38 +0600

    commit

--- MVC_Architecture/index.js
+++ MVC_Architecture/index.js
@@ -6,7 +6,6 @@ const userRouter = require('./routes/user-route')
 app.use(express.urlencoded({ extended: true }));
 
 app.use(userRouter);
-
 router.use((req, res, next) => {
     res.status(404).json({
         msg: "not found",
